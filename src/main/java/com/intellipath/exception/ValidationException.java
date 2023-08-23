67e0d023bc68b74f3972fdaf4a4728baee5d0936
Vaibhav <138553000+1DVaibhav@users.noreply.github.com>
2023-08-24 05:20:31 +0530

    Delete src directory

--- src/main/java/com/intellipath/exception/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.intellipath.exception;
-
-public class ValidationException extends RuntimeException {
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
